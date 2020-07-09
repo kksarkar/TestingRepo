5cb5388a4f0822e8619190269d1d51e2e9746ace
KAMLESH <kamleshgujariya23@gmail.com>
2020-07-09 07:53:23 -0700

    Testing git

--- TifinBox/src/main/java/com/tifinbox/app/configure/EmailCfg.java
+++ TifinBox/src/main/java/com/tifinbox/app/configure/EmailCfg.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+//Testing git
 @Service
 public class EmailCfg {
     @Value("${spring.mail.host}")
